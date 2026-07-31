4cef6a999c243f2d9159d99daee34f0cc0dd8f02
Raspberry Pi OS <Edsherman12@gmail.com>
2026-07-31 15:34:59 -0500

    Keyboard input established

--- src/button2.py
+++ src/button2.py
@@ -1,14 +1,17 @@
 from gpiozero import Button
 from pynput.keyboard import Controller, Key
 from signal import pause
+import time
 
 button=Button(26, pull_up=True)
 keyboard=Controller()
 
 def pressed():
-keyboard.press(Key.up)
-keyboard.release(Key.up)
+    print("THE BUTTON HAS WORKED")
+    keyboard.press(Key.up)
+    time.sleep(0.05)
+    keyboard.release(Key.up)
 
 button.when_pressed= pressed
 pause()
-print("THE BUTTON HAS WORKED")
+
