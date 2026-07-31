1d028f7398ebb7649b070a02c5ee8a57cfa11ab6
eds <edsherman12@gmail.com>
2026-07-31 15:09:20 -0500

    Refactor button press handling and logging

--- src/button.py
+++ src/button.py
@@ -2,13 +2,14 @@ from gpiozero import Button
 from pynput.keyboard import Controller, Key
 from signal import pause
 
-button=Button(26, pull_up=True)
-keyboard=Controller()
+button = Button(26, pull_up=True)
+keyboard = Controller()
 
 def pressed():
+    print("THE BUTTON HAS WORKED")
     keyboard.press(Key.up)
     keyboard.release(Key.up)
 
-button.when_pressed= pressed
+button.when_pressed = pressed
+
 pause()
-print("THE BUTTON HAS WORKED")
