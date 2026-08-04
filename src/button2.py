366a5aea3703a60e503a51aea57c7436e010cc75
eds <edsherman12@gmail.com>
2026-08-04 18:12:01 -0500

    Added all arrow keyes

--- src/button2.py
+++ src/button2.py
@@ -3,16 +3,44 @@ from pynput.keyboard import Controller, Key
 from signal import pause
 import time
 
-button=Button(26, pull_up=True)
+button0=Button(26, pull_up=True)
+button1=Button(13, pull_up=True)
+button2=Button(6, pull_up=True)
+button3=Button(5, pull_up=True)
 keyboard=Controller()
 
-def pressed():
+def pressed0():
     print("THE BUTTON HAS WORKED")
     keyboard.press(Key.up)
-def released():
+def released0():
 	print("THE BUTTON HAS RELEASED")
 	keyboard.release(Key.up)
-button.when_pressed= pressed
-button.when_released=released
+def pressed1():
+    print("THE BUTTON HAS WORKED")
+    keyboard.press(Key.left)
+def released1():
+	print("THE BUTTON HAS RELEASED")
+	keyboard.release(Key.left)
+def pressed2():
+    print("THE BUTTON HAS WORKED")
+    keyboard.press(Key.right)
+def released2():
+	print("THE BUTTON HAS RELEASED")
+	keyboard.release(Key.right)
+def pressed3():
+    print("THE BUTTON HAS WORKED")
+    keyboard.press(Key.down)
+def released3():
+	print("THE BUTTON HAS RELEASED")
+	keyboard.release(Key.down)
+
+button0.when_pressed= pressed0
+button0.when_released=released0
+button1.when_pressed= pressed1
+button1.when_released=released1
+button2.when_pressed= pressed2
+button2.when_released=released2
+button3.when_pressed= pressed3
+button3.when_released=released3
 pause()
 
